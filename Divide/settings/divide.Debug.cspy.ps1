01834fef9c48b42827b549e2616f72774d404bd9
ydielflores <ydiel.flores@upr.edu>
2022-11-03 03:09:14 -0300

    something something

--- Divide/settings/divide.Debug.cspy.ps1
+++ Divide/settings/divide.Debug.cspy.ps1
@@ -23,9 +23,9 @@
 
 if ($debugfile -eq "")
 {
-& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.5\common\bin\cspybat" -f "C:\dev\IAR\Divide\settings\divide.Debug.general.xcl" --backend -f "C:\dev\IAR\Divide\settings\divide.Debug.driver.xcl" 
+& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.5\common\bin\cspybat" -f "C:\dev\IAR\IAR\Divide\settings\divide.Debug.general.xcl" --backend -f "C:\dev\IAR\IAR\Divide\settings\divide.Debug.driver.xcl" 
 }
 else
 {
-& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.5\common\bin\cspybat" -f "C:\dev\IAR\Divide\settings\divide.Debug.general.xcl" --debug_file=$debugfile --backend -f "C:\dev\IAR\Divide\settings\divide.Debug.driver.xcl" 
+& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.5\common\bin\cspybat" -f "C:\dev\IAR\IAR\Divide\settings\divide.Debug.general.xcl" --debug_file=$debugfile --backend -f "C:\dev\IAR\IAR\Divide\settings\divide.Debug.driver.xcl" 
 }
